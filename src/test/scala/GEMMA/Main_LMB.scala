ad56aebdc8828b36c9a84e3313d04767f8dfa3d9
Ousseynou Dione <ousseynou.dione.ext@cihbank.ma>
2025-05-13 09:59:39 +0100

    modify main

--- src/test/scala/GEMMA/Main_LMB.scala
+++ src/test/scala/GEMMA/Main_LMB.scala
@@ -11,17 +11,23 @@ import scala.language.postfixOps
 class Main_LMB  extends Simulation {
 
 private val  tpsAttente : Int = System.getProperty("tpsAttente", "0").toInt
-//private val  nbCaissesNF : Int = System.getProperty("nbCaissesNF").toInt
 private val  dureePlateau : Int =System.getProperty("dureePlateau", "1").toInt
 private val  tpsMontee : Int =System.getProperty("tpsMontee", "1").toInt
 private val  dureeTotale : Int = dureePlateau + tpsMontee + tpsAttente
 
+
+  val dateDebut: Long = System.currentTimeMillis()
 
 val httpProtocol = http
  .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
  .acceptEncodingHeader("gzip, deflate")
  .acceptLanguageHeader("fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3")
  .userAgentHeader("TESTS-DE-PERF")
+
+
+  before {
+    println(s"Début du test à ${new java.util.Date(dateDebut)}")
+  }
 
 val scnArchiveNF = scenario("ArchiveNF").exec(ArchiveNF.scn)
 
@@ -31,4 +37,10 @@ val now = Calendar.getInstance().getTime()
     scnArchiveNF.inject(atOnceUsers(1))
     .protocols(httpProtocol))
     .maxDuration(dureeTotale  minutes)
+
+
+  after {
+    val dateFin: Long = System.currentTimeMillis()
+    println(s"Fin du test à ${new java.util.Date(dateFin)}")
+  }
 }
