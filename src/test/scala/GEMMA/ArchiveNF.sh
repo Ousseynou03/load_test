386a48fe75bba1d6e58080464accd9b3026f2811
Ousseynou Dione <ousseynou.dione.ext@cihbank.ma>
2025-05-26 13:30:50 +0100

    add archive simulation

--- src/test/scala/GEMMA/ArchiveNF.sh
+++ src/test/scala/GEMMA/ArchiveNF.sh
@@ -1,33 +1,46 @@
-#!/bin/bash
+endpoint_lmb='https://gemma-perf.galerieslafayette.store/ws/rovercash/nf/archive'
 
-if [ $# -eq 4 ]
-  then
-        user=$1
-        passwordmd5=$2
-        timestamp=$3
-        body=$4
-fi
+# Traces
+# ------
+exec 2> gemma_runPostNF.log
 
-if [ $# -eq 3 ]
-  then
-        user=$1
-        passwordmd5=$2
-        timestamp=$3
-	body=''
-fi
+# Calcul du paramètre timestamp à partir de la date courante
+# ----------------------------------------------------------
+timestamp=$(date +"%s")
 
-webservice=rovercash/nf/archive
+# Calcul du hash MD5 du paramètre body
+# -------------------------------------
+body=''
 
 if [ -z "$body" ]
 then
-      bodymd5=''
+      body_md5=''
 else
-      bodymd5=$(echo -n $body | md5sum | cut -f1 -d' ')
+      body_md5=$(echo -n $body | md5sum | cut -f1 -d' ')
 fi
 
-request=$(echo POST:/$webservice?body=$bodymd5\&timestamp=$timestamp\&user=$user) 
-requesthmac=$(echo -n $request |openssl dgst -sha256 -hmac $passwordmd5 |sed 's/^.* //')
-signature=$(echo -n $requesthmac |base64 -w 0 )
-retour=$(echo -n body=$bodymd5\&timestamp=$timestamp\&user=$user\&signature=$signature)
+# User+password
+# ------------
+user='edi17406689565'
+password_md5='$2y$10$fhnNan2dzUC0KstCLk0DB.pcmiL.cGm8IMdkueDubmSIl/DFk0OK2'
 
-echo $retour
+request=$(echo POST:/rovercash/nf/archive?body=$body_md5\&timestamp=$timestamp\&user=$user)
+
+# Calcul du hash MD5 de la requête HTTP
+request_hmac=$(echo -n $request | openssl dgst -sha256 -hmac $password_md5 | sed 's/^.* //')
+
+# Calcul du paramètre signature qui correspond au scellement applicatif
+signature=$(echo -n  $request_hmac | base64 -w 0)
+
+# Construction de la requêtte d'appel finale du WS LMB à monitorer
+endpoint_ws=$(echo $endpoint_lmb?body=\&timestamp=$timestamp\&user=$user\&signature=$signature)
+
+# Lancement du CURL
+# -----------------
+echo curl -X POST "$endpoint_ws" -H 'Content-Type: multipart/form-data;' -F id_terminal=10001 -F 'filedata=@/apps/lmb/tmp/PACK_NF/ARCHIVE_001_20250505000000.zip' -o gemma_runPostNF.out --stderr gemma_runPostNF.err > ./trace.log 2>&1
+
+curl    -X POST "$endpoint_ws" \
+        -H 'Content-Type: multipart/form-data;' \
+        -F id_terminal=10001 \
+        -F 'filedata=@/apps/lmb/tmp/PACK_NF/ARCHIVE_001_20250505000000.zip' \
+        -o gemma_runPostNF.out --stderr gemma_runPostNF.err
---
+++ src/test/scala/GEMMA/ArchiveSimulation.scala
@@ -0,0 +1,52 @@
+package GEMMA
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import java.util.Base64
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+import java.security.MessageDigest
+
+class ArchiveSimulation extends Simulation {
+
+  val httpProtocol = http
+    .baseUrl("https://gemma-perf.galerieslafayette.store/ws")
+
+  val user = "edi17406689565"
+  val passwordMd5 = "$2y$10$fhnNan2dzUC0KstCLk0DB.pcmiL.cGm8IMdkueDubmSIl/DFk0OK2"
+  val timestamp = System.currentTimeMillis() / 1000
+  val body = ""
+
+  // Debug: Print timestamp
+  println(s"[DEBUG] Timestamp: $timestamp")
+
+  val bodyMd5 = if (body.isEmpty) "" else {
+    val md = MessageDigest.getInstance("MD5")
+    md.digest(body.getBytes("UTF-8")).map("%02x".format(_)).mkString
+  }
+
+  val request = s"POST:/rovercash/nf/archive?body=$bodyMd5&timestamp=$timestamp&user=$user"
+
+  val secretKey = new SecretKeySpec(passwordMd5.getBytes("UTF-8"), "HmacSHA256")
+  val mac = Mac.getInstance("HmacSHA256")
+  mac.init(secretKey)
+  val requestHmac = mac.doFinal(request.getBytes("UTF-8"))
+  val signature = Base64.getEncoder.encodeToString(requestHmac)
+
+  // Debug: Print signature
+  println(s"[DEBUG] Signature: $signature")
+
+  val endpointWs = s"/rovercash/nf/archive?body=&timestamp=$timestamp&user=$user&signature=$signature"
+
+  val scn = scenario("Archive Scenario")
+    .exec(
+      http("Post Archive")
+        .post(endpointWs)
+        .header("Content-Type", "multipart/form-data")
+        .formParam("id_terminal", "10001")
+        .formUpload("filedata", "src/test/resources/Archirve/ARCHIVE_001_20250505000000.zip")
+    )
+
+  setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
+}
+
